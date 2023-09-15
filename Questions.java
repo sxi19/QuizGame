9014b0f10c57125231d0c802116b49d10e42e4af
sxi19 <145152362+sxi19@users.noreply.github.com>
2023-09-15 21:38:45 +0200

    Questions.java

--- Questions.java
+++ Questions.java
@@ -1,5 +1,5 @@
 /**
- Contains an array of answers corresponding to the questions and methods to access them
+ Contains an array of questions and methods to access them
 */
 public class Questions{
   public static void main(String[], args){
