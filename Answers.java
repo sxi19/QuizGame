78ef5675f05e104aa75133e69c756dbb6c21f5d9
sxi19 <xin.sun@tu-clausthal.de>
2023-09-15 23:23:54 +0200

    this is push from local file

--- Answers.java
+++ Answers.java
@@ -2,6 +2,8 @@ class Answers{
   public static void main(String[], args){
       String[] answers = {"Xin Sun", "Shanghai", 
                           "yes, I like crow"，"I meet crow everyday"};
+		
+		system.out.println(answers[0]);
     
   }
 }
--- Questions.java
+++ Questions.java
@@ -1,10 +1,14 @@
 /**
  Contains an array of questions and methods to access them
 */
+import javautl.*
 public class Questions{
   public static void main(String[], args){
     String[] question = {"what's your name?", "where are u from?", 
                          "do you like bird?","do you like crow?"};
+   
+   Scanner in = new Scanner (system in);
+   String type = in.nextStr();
    
   }
 }
